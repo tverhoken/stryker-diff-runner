93b89cd16156fcb7d23045431ecfe976d1c43d63
Thomas VERHOKEN <tverhoken@norauto.com>
2019-03-14 16:46:30 +0100

    Init project with basic usage

---
+++ src/index.ts
@@ -0,0 +1,79 @@
+import { exec } from 'child_process';
+import { Config } from 'stryker-api/config';
+import Stryker from 'stryker';
+import { join } from 'path';
+
+export default function run(commandArgs: string[]) {
+  const strykerConfPath = join(process.cwd(), 'stryker.conf.js');
+
+  exec('git diff origin/master --name-only | grep -E -v \'.*\\.test.*\' | grep -e \'src/.*\\.ts\'', (error, stdout, stderr) => {
+    if (error) {
+      console.error(error.message);
+      console.error(stderr);
+      process.exit(1);
+      return;
+    }
+
+    const args = commandArgs.slice(2);
+
+    const filesToMutate = stdout.split('\n');
+    filesToMutate.splice(filesToMutate.length - 1, 1);
+
+    import(strykerConfPath)
+      .then(initConfig)
+      .then(applyArgumentsFromCommandLine(args))
+      .then(applyFilesToMutate(filesToMutate))
+      .then(launchStryker);
+  });
+}
+
+function filterExcludedFilames(fileNames: string[], matchers: string[]) {
+  const exclusionMatchers = matchers
+    .filter(matcher => matcher[0] === '!')
+    .map(matcher => matcher.substr(1));
+  return fileNames.filter(fileName => (
+    exclusionMatchers.every(matcher => fileName !== matcher)
+  ));
+}
+
+function initConfig(module: any) {
+  const config = new Config();
+  module.default(config);
+  return config;
+}
+
+function applyArgumentsFromCommandLine(args: string[]) {
+  return (config: Config) => {
+    args.reduce<Array<Array<string | number>>>((reducer, currentValue, currentIdx) => {
+      const newReducer = [...reducer];
+      if (currentIdx % 2 === 0) {
+        newReducer.push([currentValue.slice(2)]);
+      } else {
+        if (Number.isNaN(parseInt(currentValue, 10))) {
+          newReducer[reducer.length - 1].push(currentValue);
+        } else {
+          newReducer[reducer.length - 1].push(parseInt(currentValue, 10));
+        }
+      }
+      return newReducer;
+    }, []).forEach(element => {
+      config[element[0]] = element[1];
+    });
+    return config;
+  };
+}
+
+function applyFilesToMutate(filesToMutate: string[]) {
+  return (config: Config) => {
+    const mutate = config.mutate;
+    mutate.splice(0, 1);
+    const filteredFilesToMutate = filterExcludedFilames(filesToMutate, mutate);
+    return { ...config, mutate: mutate.concat(filteredFilesToMutate) } as Config;
+  };
+}
+
+function launchStryker(config: Config) {
+  const stryker = new Stryker(config);
+
+  return stryker.runMutationTest();
+}
---
+++ tests/index.test.ts
@@ -0,0 +1,196 @@
+import Stryker from 'stryker';
+import { Config } from 'stryker-api/config';
+import { exec } from 'child_process';
+
+import run from '@src/index';
+import defaultStrykerConfFile from '../stryker.conf.js';
+
+jest.mock('child_process');
+jest.mock('stryker');
+jest.mock('stryker-api/config');
+jest.mock('../stryker.conf.js');
+
+describe('Stryker diff runner', () => {
+  let mockedConfig: any;
+  let mockedStrykerInstance: any;
+  beforeEach(() => {
+    mockedConfig = {
+      mutate: []
+    };
+    mockConfigConstruction(mockedConfig);
+
+    mockedStrykerInstance = {
+      runMutationTest: jest.fn()
+    };
+    mockStrykerConstruction(mockedConfig);
+  });
+
+  it('Should exit when file diff command gives an error.', () => {
+    const exitMock = jest.spyOn(process, 'exit').mockImplementation();
+
+    run(['node', 'exec']);
+
+    runFileDiffCommandCallback(new Error('NOPE'), '');
+
+    expect(exitMock).toHaveBeenCalledWith(1);
+    expect(defaultStrykerConfFile).not.toHaveBeenCalled();
+  });
+
+  it('Should import "stryker.conf.js" from project directory by default.', done => {
+    run(['node', 'exec']);
+
+    runFileDiffCommandCallback(null, '');
+
+    setTimeout(() => {
+      expect(defaultStrykerConfFile).toHaveBeenCalledWith(mockedConfig);
+      done();
+    });
+  });
+
+  it('Should run mutation test with loaded configuration when no args are provided to the run.', done => {
+    run(['node', 'exec']);
+
+    runFileDiffCommandCallback(null, '');
+
+    setTimeout(() => {
+      expect(mockedStrykerInstance.runMutationTest).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  it('Should remove first line of the "mutate" config properties in order to avoid running stryker to full match files for mutations.', done => {
+    const mockedConfig = {
+      mutate: ['files/to/apply/mutations/regex']
+    };
+    const expectedConfig = {
+      mutate: []
+    };
+    mockConfigConstruction(mockedConfig);
+    mockStrykerConstruction(expectedConfig);
+
+    run(['node', 'exec']);
+
+    runFileDiffCommandCallback(null, '');
+
+    setTimeout(() => {
+      expect(mockedStrykerInstance.runMutationTest).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  it('Should concat file list from diff command to "mutate" config property.', done => {
+    const fileDiffList = 'file1\nfile2\nfile3\n';
+    const mockedConfig = {
+      mutate: ['files/to/apply/mutations/regex']
+    };
+    const expectedConfig = {
+      mutate: ['file1', 'file2', 'file3']
+    };
+    mockConfigConstruction(mockedConfig);
+    mockStrykerConstruction(expectedConfig);
+    run(['node', 'exec']);
+
+    runFileDiffCommandCallback(null, fileDiffList);
+
+    setTimeout(() => {
+      expect(mockedStrykerInstance.runMutationTest).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  it('Should exclude files from list diff command that are in the exclusion of the "mutate" config property.', done => {
+    const fileDiffList = 'file1\nfile2\nfile3\n';
+    const mockedConfig = {
+      mutate: ['files/to/apply/mutations/regex', '!file3']
+    };
+    const expectedConfig = {
+      mutate: ['!file3', 'file1', 'file2']
+    };
+    mockConfigConstruction(mockedConfig);
+    mockStrykerConstruction(expectedConfig);
+
+    run(['node', 'exec']);
+
+    runFileDiffCommandCallback(null, fileDiffList);
+
+    setTimeout(() => {
+      expect(mockedStrykerInstance.runMutationTest).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  it('Should concat command arguments and values to stryker configuration.', done => {
+    const expectedConfig = {
+      mutate: [],
+      arg1: 'arg1Value',
+      arg2: 'arg2Value'
+    };
+    mockStrykerConstruction(expectedConfig);
+
+    run(['node', 'exec', '--arg1', 'arg1Value', '--arg2', 'arg2Value']);
+
+    runFileDiffCommandCallback(null, '');
+
+    setTimeout(() => {
+      expect(mockedStrykerInstance.runMutationTest).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  it('Should concat command arguments and numeric values as numeric values to stryker configuration.', done => {
+    const expectedConfig = {
+      mutate: [],
+      arg1: 1,
+      arg2: 'arg2Value'
+    };
+    mockStrykerConstruction(expectedConfig);
+
+    run(['node', 'exec', '--arg1', '1', '--arg2', 'arg2Value']);
+
+    runFileDiffCommandCallback(null, '');
+
+    setTimeout(() => {
+      expect(mockedStrykerInstance.runMutationTest).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  it('Should concat command arguments and values to stryker configuration and replace existing ones.', done => {
+    const mockedConfig = {
+      mutate: [],
+      arg1: 'notArg1Value'
+    };
+    const expectedConfig = {
+      mutate: [],
+      arg1: 'arg1Value',
+      arg2: 'arg2Value'
+    };
+    mockConfigConstruction(mockedConfig);
+    mockStrykerConstruction(expectedConfig);
+    
+    run(['node', 'exec', '--arg1', 'arg1Value', '--arg2', 'arg2Value']);
+
+    runFileDiffCommandCallback(null, '');
+
+    setTimeout(() => {
+      expect(mockedStrykerInstance.runMutationTest).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  function mockStrykerConstruction(expectedConfig: any) {
+    (Stryker as jest.Mock).mockImplementation(config => {
+      if (JSON.stringify(config) === JSON.stringify(expectedConfig)) {
+        return mockedStrykerInstance;
+      }
+    });
+  }
+
+  function mockConfigConstruction(mockedConfig: any) {
+    (Config as jest.Mock).mockImplementation(() => mockedConfig);
+  }
+
+  function runFileDiffCommandCallback(error: Error | null, fileDiffList: string) {
+    (exec as any).mock.calls[0][1](error, fileDiffList);
+  }
+});
