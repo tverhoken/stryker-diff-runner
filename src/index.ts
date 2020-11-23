0dfbee5d2ca96f9c2a7b7bc5fbef1fb837eed5e7
Thomas VERHOKEN <thomas.verhoken@gmail.com>
2020-11-23 14:05:38 +0100

    feat(conf): Enable json configuration file use (#32)
    - Update nehavior to lookup for a `stryker.conf.json` file when no JS configuration file is found.
    - Upgrade dev dependencies to latest versions.
    - Fix `package.json` "main" property.
    
    Closes #30

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,22 @@ import { StrykerOptions } from "@stryker-mutator/api/core";
 import Stryker from "@stryker-mutator/core";
 import { exec } from "child_process";
 import { join } from "path";
+import { existsSync } from "fs";
 
 export default function run(commandArgs: string[]) {
 
   const args = commandArgs.slice(2);
   const argsValuesArray = mapAllArgumentsToAnArray(args);
-  const branchArgPredicate = (argArray: Array<string | number>) => argArray[0] === "branch";
-  const pathArgPredicate = (argArray: Array<string | number>) => argArray[0] === "path";
+  const branchArgPredicate = (argArray: (string | number)[]) => argArray[0] === "branch";
+  const pathArgPredicate = (argArray: (string | number)[]) => argArray[0] === "path";
 
   const branch = argsValuesArray.some(branchArgPredicate) && argsValuesArray.find(branchArgPredicate)![1].toString();
   const path = argsValuesArray.some(pathArgPredicate) && argsValuesArray.find(pathArgPredicate)![1].toString();
 
-  const strykerConfPath = join(process.cwd(), path || "stryker.conf.js");
+  let strykerConfPath = join(process.cwd(), path || "stryker.conf.js");
+  if (!existsSync(strykerConfPath)) {
+    strykerConfPath = join(process.cwd(), path || "stryker.conf.json");
+  }
 
   exec(
     `git diff origin/${branch || "master"} --name-only | grep -E -v '.*\\.test.*' | grep -e 'src/.*\\.[jt]s'`,
@@ -60,7 +64,7 @@ function applyArgumentsFromCommandLine(args: string[]) {
 }
 
 function mapAllArgumentsToAnArray(args: string[]) {
-  return args.reduce<Array<Array<string | number>>>((reducer, currentValue, currentIdx) => {
+  return args.reduce<(string | number)[][]>((reducer, currentValue, currentIdx) => {
     const newReducer = [...reducer];
     if (currentIdx % 2 === 0) {
       newReducer.push([currentValue.slice(2)]);
